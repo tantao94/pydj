726521003e8a357e1a9ce2eda0a2b394ecac65cd
unknown <>
2016-08-01 21:41:29 +0800

    add blog

--- myweb/blog/views.py
+++ myweb/blog/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from .models import Blog
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    blog_list = Blog.objects.all()
+    return render_to_response('index.html',{'blogs':blog_list})
+
+# def login(request):
+#     username=request.POST.get('username','')
+#     password=request.POST.get('password','')
+#     if username == 'sunlin' and password == '123456':
+#         return HttpResponse('login success')
+#     else:
+#         return render_to_response('index.html',{'error':'username and password error'})
